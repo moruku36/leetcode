4ee1748ee8d1aa8759b2d45ceb5101fd2c2487fb
SeaOtter(Ken) <kentaro.mori36@gmail.com>
2022-02-11 08:07:16 +0900

    2022/02/11
    2022/02/11

---
+++ Kids With the Greatest Number of Candies.py
@@ -0,0 +1,11 @@
+class Solution:
+    def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
+        num = max(candies)
+        for i in range(len(candies)):
+            if candies[i] + extraCandies >= num:
+                candies[i] = True
+            else:
+                candies[i] = False
+        return candies
+# Runtime: 64 ms, faster than 33.33% of Python3 online submissions for Kids With the Greatest Number of Candies.
+# Memory Usage: 13.6 MB, less than 100.00% of Python3 online submissions for Kids With the Greatest Number of Candies.
