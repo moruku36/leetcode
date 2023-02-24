a105eddaf0ceb92ed808bd9dadb8a58870143b25
SeaOtter(Ken) <kentaro.mori36@gmail.com>
2023-02-25 08:45:30 +0900

    Add Find Pivot Index.py and update README.md

--- LinkedListCycle.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def hasCycle(self, head: ListNode) -> bool:
-        nodes_seen = set()
-        while head is not None:
-            if head in nodes_seen:
-                return True
-            nodes_seen.add(head)
-            head = head.next
-        return False
