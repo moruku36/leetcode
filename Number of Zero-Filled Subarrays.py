512a3620e21be2acbf8399876a533a9c34dff265
SeaOtter ( Kentaro Mori ) <kentaro.mori36@gmail.com>
2023-03-21 00:46:41 +0000

    new leetcode

---
+++ Number of Zero-Filled Subarrays.py
@@ -0,0 +1,12 @@
+class Solution:
+    def zeroFilledSubarray(self, nums: List[int]) -> int:
+        count = 0
+        zero_count = 0
+        for num in nums:
+            if num == 0:
+                zero_count += 1
+            else:
+                count += (zero_count * (zero_count + 1)) // 2
+                zero_count = 0
+        count += (zero_count * (zero_count + 1)) // 2
+        return count
